af34f3216abdb220530622a582d3e70f2c3b6b96
William Dupont <williamd.contact@gmail.com>
2022-09-13 18:01:19 +0100

    Initial Commit

---
+++ QRPeeps/QRPeepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRPeepsApp.swift
+//  QRPeeps
+//
+//  Created by William Dupont on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct QRPeepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
