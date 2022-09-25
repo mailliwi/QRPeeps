70006a22db5e8a46dc48b497010a4965d7b3b147
William Dupont <williamd.contact@gmail.com>
2022-09-25 22:41:40 +0100

    Start working on email functionality

--- QRPeeps/Components/PeepHeader.swift
+++ QRPeeps/Components/PeepHeader.swift
@@ -10,21 +10,39 @@ import SwiftUI
 struct PeepHeader: View {
     let peep: Peep
     
+    func openEmailApp(toEmail: String, subject: String, body: String) {
+        guard
+            let subject = subject.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
+            let body = "Just testing ...".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+        else {
+            print("Error: Can't encode subject or body.")
+            return
+        }
+        
+        let urlString = "mailto:\(toEmail)?subject=\(subject)&body=\(body)"
+        let url = URL(string:urlString)!
+        
+        UIApplication.shared.open(url)
+    }
+    
     var body: some View {
         VStack {
             DefaultProfilePicture()
             Text(peep.name)
                 .font(.system(.largeTitle, design: .rounded))
                 .fontWeight(.bold)
-            Text(peep.emailAddress)
-                .font(.body)
-                .textCase(.lowercase)
-                .foregroundColor(.secondary)
+            Button {
+                openEmailApp(toEmail: peep.emailAddress, subject: "Mail test subject", body: "Mail test body")
+            } label: {
+                Label(peep.emailAddress, systemImage: "envelope")
+                    .textCase(.lowercase)
+            }
         }
     }
 }
 
 struct PeepHeader_Previews: PreviewProvider {
+    func onTap() -> Void { return }
     static var previews: some View {
         PeepHeader(peep: Peep.peeper)
     }
