f928757981596d5ddf3c2e9d1c5b3924e8dec9bb
William Dupont <williamd.contact@gmail.com>
2022-10-02 19:28:23 +0100

    Add peep's profile picture to PeepCard component

--- QRPeeps/Components/PeepCard.swift
+++ QRPeeps/Components/PeepCard.swift
@@ -14,6 +14,22 @@ struct PeepCard: View {
     
     var body: some View {
         HStack {
+            if peep.image == nil {
+                Image(systemName: "person")
+                    .frame(width: 32, height: 32)
+                    .font(.system(size: 16))
+                    .foregroundColor(.gray)
+                    .background(.white)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(.gray, lineWidth: 1))
+            } else {
+                Image(uiImage: UIImage(data: peep.image!)!)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 32, height: 32)
+                    .clipShape(Circle())
+                
+            }
             VStack(alignment: .leading) {
                 Text(peep.name)
                 Text(peep.emailAddress)
